39f1f15b0bbcd26d1128045f03aee16d4a9c153b
Marcos <mbinoti@gmail.com>
2023-06-28 14:14:27 -0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
+import 'package:appclimatempo/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
     );
   }
 }
